83fa510c98c9942db13f0b242f8f571ef3da1078
Twenty Eyghth <28oi>
2026-05-04 16:11:06 +0300

    Добавлен интерфейс ITask

---
+++ ITask.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DailyPlanner
+{
+    public interface ITask
+    {
+        string Title { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+        bool IsCompleted { get; set; }
+        
+        string GetDescription();
+    }
+}
